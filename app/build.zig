14f665760bc840e182bd345e6a4845d030ecfac8
Jonatan Czarniecki <czarniecki.jonatan@gmail.com>
2026-01-16 21:06:11 +0100

    implement a proof of concept plugin loader

---
+++ app/build.zig
@@ -0,0 +1,121 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+	const target = b.standardTargetOptions(.{});
+	const optimize = b.standardOptimizeOption(.{});
+
+	// Include the dependencies.
+
+	// the plugin API
+
+	const libplugskaler_api = b.dependency("plugskaler_api", .{
+		.target = target,
+		.optimize = optimize,
+	});
+	const plugskaler_api = libplugskaler_api.module("plugskaler-api");
+
+	// built-in plugins
+
+	const opt_build_plugins = b.option(bool, "build-plugins", "Build the plugins and install them as artifacts") orelse false;
+	if (opt_build_plugins) {
+		// Include the lazy dependencies
+
+		const libplugskaler_realcugan = b.lazyDependency("plugskaler_realcugan", .{
+			.target = target,
+			.optimize = optimize,
+		});
+		const libplugskaler_waifu2x = b.lazyDependency("plugskaler_waifu2x", .{
+			.target = target,
+			.optimize = optimize,
+		});
+
+		// Install the dynamic libraries.
+
+		b.installArtifact(libplugskaler_realcugan.?.artifact("plugskaler-realcugan"));
+		b.installArtifact(libplugskaler_waifu2x.?.artifact("plugskaler-waifu2x"));
+	}
+
+	// Define the library module of the app (unused for now).
+
+	const mod = b.addModule("upskaler", .{
+		.root_source_file = b.path("root.zig"),
+		.target = target,
+		.imports = &.{
+			// might need access to the API later
+			// .{ .name = "plugskaler-api", .module = plugskaler_api },
+		},
+	});
+
+	// Define the executable.
+
+	const exe = b.addExecutable(.{
+		.name = "upskaler",
+		.root_module = b.createModule(.{
+			.root_source_file = b.path("main.zig"),
+			.target = target,
+			.optimize = optimize,
+			.imports = &.{
+				.{ .name = "upskaler", .module = mod }, // the library module of the app
+				.{ .name = "plugskaler-api", .module = plugskaler_api }, // the plugin API
+			},
+		}),
+	});
+
+	// Install the executable.
+
+	b.installArtifact(exe);
+
+	// Make the app executable by simply running `zig build run`.
+
+	const run_step = b.step("run", "Run the app");
+
+	// Use the built executable artifact for `zig build run`.
+
+	const run_cmd = b.addRunArtifact(exe);
+	run_step.dependOn(&run_cmd.step);
+	run_cmd.step.dependOn(b.getInstallStep()); // We need to install the executable and plugins before using them
+	if (b.args) |args| {
+		run_cmd.addArgs(args); // Pass the arguments to the executable.
+	}
+
+	//TODO: configure and use tests
+
+	// Creates an executable that will run `test` blocks from the provided module.
+	// Here `mod` needs to define a target, which is why earlier we made sure to
+	// set the releative field.
+	const mod_tests = b.addTest(.{
+			.root_module = mod,
+	});
+
+	// A run step that will run the test executable.
+	const run_mod_tests = b.addRunArtifact(mod_tests);
+
+	// Creates an executable that will run `test` blocks from the executable's
+	// root module. Note that test executables only test one module at a time,
+	// hence why we have to create two separate ones.
+	const exe_tests = b.addTest(.{
+			.root_module = exe.root_module,
+	});
+
+	// A run step that will run the second test executable.
+	const run_exe_tests = b.addRunArtifact(exe_tests);
+
+	// A top level step for running all tests. dependOn can be called multiple
+	// times and since the two run steps do not depend on one another, this will
+	// make the two of them run in parallel.
+	const test_step = b.step("test", "Run tests");
+	test_step.dependOn(&run_mod_tests.step);
+	test_step.dependOn(&run_exe_tests.step);
+
+	// Just like flags, top level steps are also listed in the `--help` menu.
+	//
+	// The Zig build system is entirely implemented in userland, which means
+	// that it cannot hook into private compiler APIs. All compilation work
+	// orchestrated by the build system will result in other Zig compiler
+	// subcommands being invoked with the right flags defined. You can observe
+	// these invocations when one fails (or you pass a flag to increase
+	// verbosity) to validate assumptions and diagnose problems.
+	//
+	// Lastly, the Zig build system is relatively simple and self-contained,
+	// and reading its source code will allow you to master it.
+}
---
+++ app/main.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+const UpskalerPlugin = @import("plugskaler-api").UpskalerPlugin;
+
+const PluginManager = struct {
+
+	lib: std.DynLib,
+	plugin: *UpskalerPlugin,
+
+	const Self = @This();
+	const Error = std.DynLib.Error || error{ SymbolNotFound };
+
+	pub fn init(path: []const u8) Error!Self {
+		var lib = try std.DynLib.open(path);
+		return Self{
+			.lib = lib,
+			.plugin = lib.lookup(*UpskalerPlugin, "plugin") orelse {
+				defer lib.close();
+				return error.SymbolNotFound;
+			},
+		};
+	}
+
+	pub fn deinit(self: *Self) void {
+		self.lib.close();
+	}
+};
+
+pub fn main() !void {
+	var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+	defer arena.deinit();
+
+	var plugin_registry = std.StringHashMap(PluginManager).init(arena.allocator());
+	defer plugin_registry.deinit();
+
+	var gp = std.heap.GeneralPurposeAllocator(.{}).init;
+	const gpa = gp.allocator();
+	defer _ = gp.deinit();
+
+	const exe_path = try std.fs.selfExeDirPathAlloc(gpa);
+	defer gpa.free(exe_path);
+	const plugin_path_base = try std.fs.path.join(gpa, &[_][]const u8{ exe_path, "..", "lib" });
+	defer gpa.free(plugin_path_base);
+
+	const plugins = [_][]const u8{ "realcugan", "waifu2x" };
+	inline for(plugins) |name| {
+		const res = try plugin_registry.getOrPut(name);
+		if (res.found_existing) {
+			std.debug.print("Refusing to register duplicate plugin.\n", .{});
+			return;
+		}
+		res.key_ptr.* = name;
+
+		const plugin_path = try std.fs.path.join(gpa, &[_][]const u8{ plugin_path_base, "/libplugskaler-" ++ name ++ ".so" });
+		defer gpa.free(plugin_path);
+		res.value_ptr.* = try PluginManager.init(plugin_path);
+	}
+	defer {
+		var it = plugin_registry.valueIterator();
+		while (it.next()) |pm| {
+			pm.deinit();
+		}
+	}
+
+	var it = plugin_registry.valueIterator();
+	while (it.next()) |pm| {
+		std.debug.print("test_int: {}\n", .{ pm.plugin.test_int });
+	}
+}
